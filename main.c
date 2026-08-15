6ec66955505d00011f688d1a1786a85be1b69119
Grigorii <grigory.bocharsky@gmail.com>
2026-08-15 20:56:00 +0300

    Added execution of non-built in commands

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_INPUT   1024  
 #define MAX_TOKENS  32
@@ -68,6 +69,60 @@ int executeBuiltInCommand(char **args){
     return 0;
 }
 
+int checkPipe(char **args){
+    for(int i = 0; args[i] != NULL; i++){
+        if(strcmp(args[i], "|") == 0)
+            return 1;
+    }
+
+    return 0;
+}
+
+void seperatePipeCommands(char **args, char **args1, char **args2){
+    int i, j;
+
+    for(i = 0; strcmp(args[i], "|") != 0; i++){
+        for(j = 0; args[i][j]; j++){
+            args1[i][j] = args[i][j];
+        }
+        args1[i][strcspn(args1[i], "\n")] = '\0';
+    }
+
+    for(i = i + 1; args[i]; i++){
+        for(j = 0; args[i][j]; j++){
+            args2[i][j] = args[i][j];
+        }
+        args2[i][strcspn(args2[i], "\n")] = '\0';
+    }
+}
+
+int executePipe(char **args){
+    char **args1, **args2;
+
+    seperatePipeCommands(args, args1, args2);
+    
+    return 0;
+}
+
+int executeCustomCommand(char **args){
+    pid_t pid = fork();
+
+    switch (pid)
+    {
+    case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+    case 0:
+        if(execvp(args[0], args) < 0){
+            perror("execution failed");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    default:
+        wait(NULL);
+    }
+
+}
 
 
 
@@ -79,7 +134,14 @@ int main(){
         printDir();
         readInput(input);
         tokenize(input, tokens);
-        executeBuiltInCommand(tokens);
+        if(checkPipe(tokens)){
+            executePipe(tokens);
+        }
+        else{
+            if(!executeBuiltInCommand(tokens))
+                executeCustomCommand(tokens);
+        }
+
     }
 
     return 0;
